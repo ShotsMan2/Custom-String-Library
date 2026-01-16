659656b0851b341a19a8a1ec15eb58e61210281f
Selim Efe <uysalselimefe@gmail.com>
2026-01-16 14:25:13 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,30 +2,30 @@
 #include <stdlib.h>
 #include <time.h>
 
-void* SUBSTRING(char yazi[], int baslangicIndex, int uzunluk,char sonuc[]){
-    int i,j=0;
+void *SUBSTRING(char yazi[], int baslangicIndex, int uzunluk, char sonuc[])
+{
+    int i, j = 0;
 
-    for(i=baslangicIndex;i<baslangicIndex+uzunluk;i++){
-        sonuc[j]=yazi[i];
+    for (i = baslangicIndex; i < baslangicIndex + uzunluk; i++)
+    {
+        sonuc[j] = yazi[i];
         j++;
     }
 
-    sonuc[j]='\0';
+    sonuc[j] = '\0';
 
     return sonuc;
-
-
 }
 
 int main()
 {
     char sonuc[99];
-     int baslangicIndex=3,uzunluk=99,i;
+    int baslangicIndex = 3, uzunluk = 99, i;
 
-    SUBSTRING("selimefeuysal", baslangicIndex, uzunluk,sonuc);
+    SUBSTRING("selimefeuysal", baslangicIndex, uzunluk, sonuc);
 
-    for(i=0;i<uzunluk;i++){
-        printf("%c",sonuc[i]);
+    for (i = 0; i < uzunluk; i++)
+    {
+        printf("%c", sonuc[i]);
     }
-
 }
